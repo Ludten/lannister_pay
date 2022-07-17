1d5038324c13809dc9f6730f1d8c035042f4aaa9
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-07-17 16:17:39 +0100

    used compression

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const compression = require('compression');
+
 const express = require("express");
 
 const IndexRouter = require("./controllers/v1/index.router");
@@ -7,12 +9,15 @@ const bodyParser = require('body-parser');
 (async () => {
   const app = express();
 
+  app.use(compression());
+
   //port listen
   const port = process.env.PORT || 3000;
 
   app.use(bodyParser.urlencoded({ extended: false }))
       .use(bodyParser.json())
       .use('/', IndexRouter);
+
 
 
   // Start the Server
